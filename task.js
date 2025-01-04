113f29d851742b85f79250a6b7338d1f83d75940
manishkumarsingh2302 <manishkumarbkj2005@gmail.com>
2025-01-04 19:05:48 +0530

    succes

---
+++ task.js
@@ -0,0 +1,15 @@
+let lis=document.getElementsByTagName("li")
+for(let a=0;a>10;a++){
+    if (a%2==0)
+    {
+        lis[a].style.backgroundColor="yellow"
+    }
+}
+let head=Document.createElement("h1")
+    head.textContent="hi we are developers...!!"
+    let bd=Document.body 
+    bd.apped(head)
+    bd.setAttribute('bgcolor','red')
+    head.setAttribute('align','center')
+    let as_nm=document.getElementById('head2')
+    as_nm.removeAttribute('align')
