010ca26ca6b082886d8815c8ce8d42068881cfb6
manishkumarsingh2302 <manishkumarbkj2005@gmail.com>
2025-01-06 19:01:35 +0530

    smile

---
+++ dom4.js
@@ -0,0 +1,20 @@
+function fun2(){
+    let image=document.getElementById("image")
+    image.src="dinner1.jpg"
+}
+function fun1(){
+    let image=document.getElementById("image")
+    image.src="breakfast1.jpg"
+}
+function red(){
+    document.body.style.backgroundColor="red"
+}
+function green(){
+    document.body.style.backgroundColor="green"
+}
+function pink(){
+    document.body.style.backgroundColor="pink"
+}
+function blue(){
+    document.body.style.backgroundColor="blue"
+}
