e4079e16be482c0d0c0a8e32a2e0863406562bb3
manishkumarsingh2302 <manishkumarbkj2005@gmail.com>
2025-01-31 18:56:04 +0530

    succes

---
+++ obj.js
@@ -0,0 +1,41 @@
+let person_student={
+    name :"maha raj",
+age :90,
+contact:1234,
+account_stat:{
+    ac_bal :12000,
+    account_no:12345
+},
+personal_details:{
+    marita_statues:"inmarrid",
+    do:"12jan90"
+},
+documents:[1234,"123sf"],
+fun:function(name){
+    alert("inside onject" +name)
+}
+
+}
+// console.log(person_student.account_stat.ac_bal)
+console.log(person_student.documents[1]);
+person_student.fun("manish");
+// ==================================================
+
+let insaan={
+    name:"manish",
+    age:20,
+    city:"ARA",
+    fun:function(){
+        console.log(this.name);
+    }
+}
+insaan.fun()
+// =======================================================
+
+let ob={
+    name :"manish",age:20,contact:56456454
+}
+for(let ky in ob){
+    console.log(ky);
+}
+// ===========================================================
