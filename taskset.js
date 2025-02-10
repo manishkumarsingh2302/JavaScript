0ba025e71ae420bf8b2c33b19f62d525a245288d
manishkumarsingh2302 <manishkumarbkj2005@gmail.com>
2025-02-10 18:55:54 +0530

    yes

--- taskset.js
+++ taskset.js
@@ -1,8 +1,35 @@
-let frm =document.querySelector('#frm')
-setTimeout(()=>{
-     frm.style.display='block'
-    },2000)
+// let frm =document.querySelector('#frm')
+// setTimeout(()=>{
+//      frm.style.display='block'
+//     },2000)
 
-    //=================
-    let btn=document.querySelector('.btn')
-    btn.addEventListener('click',()=>{alert("event fire")})
+//     //=================
+//     let btn=document.querySelector('.btn')
+//     btn.addEventListener('click',()=>{alert("event fire")})
+
+
+const multi=()=>{
+     let userdata={
+          name : document.querySelector('#name').value,
+          email:document.querySelector('#email').value,
+          password:document.querySelector('#password')
+     }
+     localStorage.setItem("userdata",JSON.stringify(userdata))
+}
+
+
+let data =JSON.parse(localStorage.getItem('userdata'))
+console.log(data); 
+
+//==================================================================
+
+function loginform(){
+     let logindata={
+          loginemail :document.querySelector('#loginemail').value,
+          loginpassword :document.querySelector('#loginpassword').value,
+     }
+     if(data.email !=logindata.loginemail || data.password !=logindata.loginpassword){
+          alert("user not found")
+          return false
+     }
+}
